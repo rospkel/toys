dc7a28186873ea5fdc93e83f7ab1650cc86d5cdc
Ryan <rospkel@gmail.com>
2021-06-28 20:42:05 -0400

    initialization

---
+++ password.py
@@ -0,0 +1,6 @@
+print('Enter the password:')
+password = input()
+while password != "password":
+    print('You are fake news.')
+    password = input()
+print("Congratulations. You have entered the correct password.")
