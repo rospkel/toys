1361ffb63ee27ca1096d4c69319e855874e1fa0a
Ryan <rospkel@gmail.com>
2021-06-28 21:56:52 -0400

    madlibs fix anothers

--- madlibs.py
+++ madlibs.py
@@ -1,8 +1,8 @@
 print('Choose an adjective.')
 noun1 = input()
-print('Choose another noun.')
+print('Choose a noun.')
 noun2 = input()
-print('Choose an adjective.')
+print('Choose another adjective.')
 adj1 = input()
 
 print("Your mother was a " + noun1 + " " + noun2 + ". That's why she's so " + adj1 + ".")
